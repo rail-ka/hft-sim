7425da2133a39c648230f9e683a31f73d9113ef4
Rail Khusnutdinov <rail@33.run>
2025-11-04 18:19:11 +0300

    1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{env, fs, sync::Arc, thread, time::Duration};
+use std::{collections::HashMap, env, fs, path::PathBuf, sync::Arc, thread, time::Duration};
 
 mod config;
 mod types;
 use crossbeam_queue::ArrayQueue;
-use quanta::Clock;
+use quanta::{Clock, Instant};
 
 use crate::{
     config::Config,
@@ -17,9 +17,10 @@ fn main() {
         std::process::exit(1);
     }
     let config_path = &args[1];
+    let config_path = PathBuf::from(config_path.as_str()).canonicalize().unwrap();
 
-    let config_data = fs::read_to_string(config_path).unwrap_or_else(|err| {
-        eprintln!("Failed to read config file '{}': {}", config_path, err);
+    let config_data = fs::read_to_string(&config_path).unwrap_or_else(|err| {
+        eprintln!("Failed to read config file {:?}: {}", config_path, err);
         std::process::exit(1);
     });
 
@@ -108,6 +109,10 @@ fn main() {
         let handle = thread::spawn(move || {});
         strategies.push(handle);
     }
+
+    for j in producers {
+        j.join().unwrap();
+    }
 }
 
 pub struct ProducerWorker {
@@ -118,42 +123,99 @@ pub struct ProducerWorker {
     stage: Arc<Stage1>,
 }
 
+fn timestamp() -> u64 {
+    std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .expect("time went backwards")
+        .as_nanos() as u64
+}
+
 impl ProducerWorker {
     fn run(self) {
         let Self {
             id,
             duration_secs,
             messages_per_sec,
-            distribution,
+            mut distribution,
             stage,
         } = self;
+        let ts = timestamp();
         let clock = Clock::new();
+        let instant = clock.now();
+        // let duration = instant.duration_since(std::time::UNIX_EPOCH);
+        println!("ts: {ts}");
         let mut msg = Message {
             ty: 0,
             producer_id: id,
             seq: 0,
-            timestamp: clock.raw(),
+            timestamp: ts,
         };
         let nano_per_msg: u64 = 1_000_000_000u64 / (messages_per_sec as u64);
-        for i in 0..duration_secs {
-            println!("sec: {i}");
+        println!("nano_per_msg: {nano_per_msg}");
+
+        distribution.sort_unstable_by_key(|(k, _)| *k);
+        let distribution_pattern = create_distribution_pattern(&distribution, 100);
+        println!("distribution_pattern: {distribution_pattern:?}");
+        let pattern_len = distribution_pattern.len();
+        if pattern_len == 0 {
+            println!(
+                "Error: Distribution pattern is empty. Producer {} exiting.",
+                id
+            );
+            return;
+        }
+
+        for s in 0..duration_secs {
+            println!("sec: {s}");
             let raw_time = clock.raw();
             let start_time = clock.now();
-            println!("{raw_time}, {start_time:?}");
+            println!("raw_time: {raw_time}, start_time: {start_time:?}");
 
-            for j in 0..messages_per_sec {
+            for i in 0..(messages_per_sec as usize) {
                 let target_time = raw_time + nano_per_msg;
                 let now = clock.raw();
+                let msg_type = distribution_pattern[i % pattern_len];
                 msg.seq += 1;
                 msg.timestamp = now;
+                msg.ty = msg_type;
                 stage.send(msg);
                 if now < target_time {
                     let ns = target_time - now;
                     thread::sleep(Duration::from_nanos(ns));
                 }
             }
+            // let now = clock.raw();
+            // let rem = now - raw_time;
         }
     }
+}
+
+fn create_distribution_pattern(distribution: &[(u64, f64)], pattern_size: usize) -> Vec<u64> {
+    let mut pattern = Vec::with_capacity(pattern_size);
+
+    for (msg_type, fraction) in distribution {
+        let count = (pattern_size as f64 * fraction).round() as usize;
+
+        for _ in 0..count {
+            if pattern.len() < pattern_size {
+                pattern.push(*msg_type);
+            }
+        }
+    }
+
+    // Из-за ошибок округления может не хватать/быть лишних элементов. Корректируем.
+    // Добавляем самый вероятный тип, чтобы минимизировать искажение
+    while pattern.len() < pattern_size {
+        let default_type = distribution
+            .iter()
+            .max_by(|a, b| a.1.partial_cmp(&b.1).unwrap())
+            .map(|(k, _)| *k)
+            .unwrap_or(0);
+        pattern.push(default_type);
+    }
+
+    pattern.truncate(pattern_size);
+    pattern
 }
 
 pub struct Stage1 {}
